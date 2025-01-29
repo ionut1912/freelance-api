596d470a213760dc5d4d3fe1173cbc9c79a162e1
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-29 11:40:49 +0200

    Remove unnedeed role

--- Frelance.Infrastructure/Migrations/20250129092032_RemoveAdminRole.Designer.cs
+++ Frelance.Infrastructure/Migrations/20250129092032_RemoveAdminRole.Designer.cs
@@ -4,6 +4,7 @@ using Frelance.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Frelance.Infrastructure.Migrations
 {
     [DbContext(typeof(FrelanceDbContext))]
-    partial class FrelanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250129092032_RemoveAdminRole")]
+    partial class RemoveAdminRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
