2c45aeb9a1aad023d6aceceee773286d59db7092
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-29 13:11:57 +0200

    fix app service code

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -13,16 +13,21 @@ resource "azurerm_linux_web_app" "app_service" {
   service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
-    always_on           = false
-    app_command_line    = ""
-    docker_registry_url = azurerm_container_registry.acr.login_server
-    docker_image_name   = "${azurerm_container_registry.acr.login_server}/frelance-api:latest"
+    always_on        = false
+    app_command_line = ""
+    container_registry {
+      use_managed_identity = false
+      server_url           = "https://${azurerm_container_registry.acr.login_server}"
+      username             = azurerm_container_registry.acr.admin_username
+      password_secret_name = "DOCKER_REGISTRY_SERVER_PASSWORD"
+    }
   }
 
   app_settings = {
     "DATABASE_CONNECTION_STRING" = azurerm_key_vault_secret.db_connection_string.value
-    "DOCKER_REGISTRY_SERVER_URL" = azurerm_container_registry.acr.login_server
+    "DOCKER_REGISTRY_SERVER_URL" = "https://${azurerm_container_registry.acr.login_server}"
     "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
     "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+    "WEBSITES_CONTAINER_START_TIME_LIMIT" = "1800"
   }
 }
