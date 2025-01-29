d3a86b4f4582aa2a0d8fcb263f8c8888aca9db58
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-29 13:14:32 +0200

    wyp

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -15,19 +15,26 @@ resource "azurerm_linux_web_app" "app_service" {
   site_config {
     always_on        = false
     app_command_line = ""
-    container_registry {
-      use_managed_identity = false
-      server_url           = "https://${azurerm_container_registry.acr.login_server}"
-      username             = azurerm_container_registry.acr.admin_username
-      password_secret_name = "DOCKER_REGISTRY_SERVER_PASSWORD"
-    }
+    
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/frelance-api:latest"
   }
 
   app_settings = {
-    "DATABASE_CONNECTION_STRING" = azurerm_key_vault_secret.db_connection_string.value
-    "DOCKER_REGISTRY_SERVER_URL" = "https://${azurerm_container_registry.acr.login_server}"
-    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
+    "DATABASE_CONNECTION_STRING"        = azurerm_key_vault_secret.db_connection_string.value
+    "DOCKER_REGISTRY_SERVER_URL"        = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME"   = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"   = azurerm_container_registry.acr.admin_password
     "WEBSITES_CONTAINER_START_TIME_LIMIT" = "1800"
+  }
+
+  logs {
+    http_logs {
+      file_system_level = "Verbose"
+      retention_in_days = 7
+    }
+    application_logs {
+      file_system_level = "Verbose"
+      retention_in_days = 7
+    }
   }
 }
