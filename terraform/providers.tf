9d3f2b179d6883398dd2466e669319b7bf7e375f
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-29 13:41:01 +0200

    add new provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,13 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = ">=3.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">=0.7.0"
     }
   }
-
-  required_version = ">= 1.0"
 }
 
 provider "azurerm" {
-  features {}
+  features = {}
 }
+
+provider "time" {}
