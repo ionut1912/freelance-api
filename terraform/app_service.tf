d9a8b8e099c8695559a3a230b4e8ee1738289726
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-30 15:52:54 +0200

    fix wip

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -13,26 +13,32 @@ resource "azurerm_linux_web_app" "app_service" {
   service_plan_id     = azurerm_service_plan.app_service_plan.id
   https_only          = true
 
+  container_settings {
+    image_name                = "frelance-api"
+    image_tag                 = "latest"
+    registry_login_server_url = azurerm_container_registry.acr.login_server
+    registry_username         = azurerm_container_registry.acr.admin_username
+    registry_password         = azurerm_container_registry.acr.admin_password
+    start_up_command          = ""
+  }
+
   identity {
     type = "SystemAssigned"
   }
 
   site_config {
     always_on        = false
-    app_command_line = ""
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/frelance-api:latest"
   }
 
   app_settings = {
     "DATABASE_CONNECTION_STRING"          = azurerm_key_vault_secret.db_connection_string.value
-    "JWT_TOKEN_KEY"                       = azurerm_key_vault_secret.jwt_token_key.value
+    "JWT_TOKEN_KEY"                        = azurerm_key_vault_secret.jwt_token_key.value
     "DOCKER_REGISTRY_SERVER_URL"          = "https://${azurerm_container_registry.acr.login_server}"
     "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
     "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
     "WEBSITES_CONTAINER_START_TIME_LIMIT" = "1800"
   }
 }
-
 resource "azurerm_role_assignment" "keyvault_secret_access" {
   scope                = azurerm_key_vault.keyvault.id
   role_definition_name = "Key Vault Secrets User"
