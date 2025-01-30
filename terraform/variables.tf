6a8a4907d161a484a02033bca7842f39c1dcae91
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-30 10:19:57 +0200

    Modify region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name" { default = "frelance-api" }
-variable "location" { default = "West Europe" }
+variable "location" { default = "North Europe" }
 variable "app_service_plan_sku" { default = "B1" }
 variable "sql_sku_name" { default = "Basic" }
 variable "sql_sku_tier" { default = "Basic" }
