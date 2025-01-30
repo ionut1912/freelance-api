616543a63becfddad35698a4b66834c0e4f3feae
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-30 14:18:28 +0200

    fix code

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-tf-state"
+    storage_account_name = "terraformdara"
+    container_name       = "terraform-data"
+    key                 = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
