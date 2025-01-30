108890ed61a0adb9d26edb5412105d6bca40d0db
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-30 13:30:01 +0200

    fix ocde

--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -1,9 +1,10 @@
 resource "azurerm_storage_account" "terraform_storage" {
   name                     = "frelanceterraformsa"
   resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
+  location                 = "West Europe"
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "terraform_state" {
