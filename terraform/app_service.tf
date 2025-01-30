a501976f5f9eb494b3777800e915005b92de700a
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-30 16:25:40 +0200

    fix code

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -4,6 +4,10 @@ resource "azurerm_service_plan" "app_service_plan" {
   location            = azurerm_resource_group.main.location
   sku_name            = var.app_service_plan_sku
   os_type             = "Linux"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "azurerm_linux_web_app" "app_service" {
@@ -13,34 +17,25 @@ resource "azurerm_linux_web_app" "app_service" {
   service_plan_id     = azurerm_service_plan.app_service_plan.id
   https_only          = true
 
-  container_settings {
-    image_name                = "frelance-api"
-    image_tag                 = "latest"
-    registry_login_server_url = azurerm_container_registry.acr.login_server
-    registry_username         = azurerm_container_registry.acr.admin_username
-    registry_password         = azurerm_container_registry.acr.admin_password
-    start_up_command          = ""
-  }
-
   identity {
     type = "SystemAssigned"
   }
 
   site_config {
     always_on        = false
+    app_command_line = ""
+
+    application_stack {
+      docker_image     = "${azurerm_container_registry.acr.login_server}/frelance-api"
+      docker_image_tag = "latest"
+    }
   }
 
   app_settings = {
-    "DATABASE_CONNECTION_STRING"          = azurerm_key_vault_secret.db_connection_string.value
-    "JWT_TOKEN_KEY"                        = azurerm_key_vault_secret.jwt_token_key.value
-    "DOCKER_REGISTRY_SERVER_URL"          = "https://${azurerm_container_registry.acr.login_server}"
-    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
-    "WEBSITES_CONTAINER_START_TIME_LIMIT" = "1800"
+    "DATABASE_CONNECTION_STRING"      = azurerm_key_vault_secret.db_connection_string.value
+    "JWT_TOKEN_KEY"                   = azurerm_key_vault_secret.jwt_token_key.value
+    "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.acr.admin_password
   }
-}
-resource "azurerm_role_assignment" "keyvault_secret_access" {
-  scope                = azurerm_key_vault.keyvault.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_linux_web_app.app_service.identity.0.principal_id
 }
