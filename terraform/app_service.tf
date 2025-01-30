5d14144fc01d3bc549ba80b29da346868cddd474
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-30 15:44:48 +0200

    fix code

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "app_service_plan" {
   name                = "frelance-app-service-plan"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  sku_name            = var.app_service_plan_sku # Ensure this variable is set correctly
+  sku_name            = var.app_service_plan_sku
   os_type             = "Linux"
 }
 
@@ -11,7 +11,7 @@ resource "azurerm_linux_web_app" "app_service" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   service_plan_id     = azurerm_service_plan.app_service_plan.id
-  https_only          = true
+  https_only          = true  
 
   identity {
     type = "SystemAssigned"
@@ -22,14 +22,17 @@ resource "azurerm_linux_web_app" "app_service" {
     app_command_line = ""
 
     application_stack {
-      docker_image     = "${azurerm_container_registry.acr.login_server}/frelance-api"
-      docker_image_tag = "latest"
+      docker {
+        registry_url = azurerm_container_registry.acr.login_server
+        image_name   = "frelance-api"
+        tag          = "latest"
+      }
     }
   }
 
   app_settings = {
     "DATABASE_CONNECTION_STRING"          = azurerm_key_vault_secret.db_connection_string.value
-    "JWT_TOKEN_KEY"                       = azurerm_key_vault_secret.jwt_token_key.value
+    "JWT_TOKEN_KEY"                        = azurerm_key_vault_secret.jwt_token_key.value
     "DOCKER_REGISTRY_SERVER_URL"          = "https://${azurerm_container_registry.acr.login_server}"
     "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
     "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
@@ -40,5 +43,5 @@ resource "azurerm_linux_web_app" "app_service" {
 resource "azurerm_role_assignment" "keyvault_secret_access" {
   scope                = azurerm_key_vault.keyvault.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_linux_web_app.app_service.identity[0].principal_id
+  principal_id         = azurerm_linux_web_app.app_service.identity.0.principal_id 
 }
