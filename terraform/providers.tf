1b221f3ddb3335a996cb2190475164f0bff9d2c8
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-30 13:39:35 +0200

    fix providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,4 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name   = "frelance-api"
-    storage_account_name  = "frelanceterraformsa"
-    container_name        = "terraform-state"
-    key                   = "terraform.tfstate"
-  }
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
