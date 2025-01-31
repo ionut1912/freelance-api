67bb74fe4a27dc2df5f2fdcf06d408b07666e9fe
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-01-31 11:45:48 +0200

    Revert "fix app service config"
    This reverts commit 64fb04d88acf78cefaeb1ba5b05905692bd30ac4.

--- Frelance.Infrastructure/Services/TokenService.cs
+++ Frelance.Infrastructure/Services/TokenService.cs
@@ -1,13 +1,10 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using Frelance.Infrastructure.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.Extensions.Logging;
 
 namespace Frelance.Infrastructure.Services;
 
@@ -16,33 +13,23 @@ public class TokenService
     private readonly UserManager<Users> _userManager;
     private readonly string _jwtSecretKey;
 
-    public TokenService(IConfiguration configuration, UserManager<Users> userManager, ILogger<TokenService> logger)
+    public TokenService(IConfiguration configuration, UserManager<Users> userManager)
     {
         ArgumentNullException.ThrowIfNull(configuration, nameof(configuration));
         ArgumentNullException.ThrowIfNull(userManager, nameof(userManager));
         _userManager = userManager;
 
-        var keyVaultUrl = configuration["AzureKeyVault:VaultUrl"];
         var jwtSecretName = configuration["AzureKeyVault:JWTTokenSecretName"];
-
-        if (string.IsNullOrEmpty(keyVaultUrl) || string.IsNullOrEmpty(jwtSecretName))
+        if (string.IsNullOrEmpty(jwtSecretName))
         {
-            throw new Exception("Azure Key Vault URL or JWT Secret Name is missing from configuration.");
+            throw new Exception("JWTTokenSecretName is missing from configuration.");
         }
 
-        try
-        {
-            var credential = new DefaultAzureCredential();
-            var secretClient = new SecretClient(new Uri(keyVaultUrl), credential);
+        _jwtSecretKey = configuration["JWTTokenKey"];
 
-            var secret = secretClient.GetSecret(jwtSecretName);
-            _jwtSecretKey = secret.Value.Value;
-            logger.LogInformation($"Successfully retrieved JWT secret key from Azure Key Vault.");
-        }
-        catch (Exception ex)
+        if (string.IsNullOrEmpty(_jwtSecretKey))
         {
-            logger.LogError($"Error retrieving JWT secret from Azure Key Vault: {ex.Message}");
-            throw;
+            throw new Exception("JWT Token Key is missing. Ensure it is loaded from Azure Key Vault.");
         }
     }
 
