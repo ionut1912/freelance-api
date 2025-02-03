8abcd8588c73625600b52c9f0f8f0b6eaebcbad6
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-03 12:45:45 +0200

    fix code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "container_registry_url" {
 output "app_service_url" {
   value = azurerm_linux_web_app.app_service.default_hostname
 }
+
+output "app_service_client_id" {
+  value = azurerm_linux_web_app.app_service.identity[0].principal_id
+  description = "Client ID of the App Service Managed Identity"
+}
