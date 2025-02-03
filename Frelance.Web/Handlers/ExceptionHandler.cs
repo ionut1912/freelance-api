0da063919a934e200e4cfc505d85ef317bb15565
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-03 14:08:44 +0200

    debug

--- Frelance.Web/Handlers/ExceptionHandler.cs
+++ Frelance.Web/Handlers/ExceptionHandler.cs
@@ -17,14 +17,14 @@ public class ExceptionHandler : IExceptionHandler
 
     private static ProblemDetails CreateProblemDetails(Exception exception)
     {
-        ProblemDetails problemDetails = exception switch
+        var problemDetails = exception switch
         {
             NotFoundException => CreateProblemDetails(StatusCodes.Status404NotFound,
                 "Not Found", exception.Message),
             CustomValidationException => CreateProblemDetails(StatusCodes.Status400BadRequest,
                 "Validation error", "One or more validation errors occurred"),
             _ => CreateProblemDetails(StatusCodes.Status500InternalServerError,
-                "Internal Server Error", "An unexpected error ocurred")
+                "Internal Server Error", exception.ToString())
         };
 
         if (exception is CustomValidationException customValidationException)
