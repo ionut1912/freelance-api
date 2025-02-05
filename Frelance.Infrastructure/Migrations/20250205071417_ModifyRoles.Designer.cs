f935771c7b02e9a434b29f7de217337d3496ac66
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-05 09:18:01 +0200

    fix origins and modify roles

--- Frelance.Infrastructure/Migrations/20250205071417_ModifyRoles.Designer.cs
+++ Frelance.Infrastructure/Migrations/20250205071417_ModifyRoles.Designer.cs
@@ -4,6 +4,7 @@ using Frelance.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Frelance.Infrastructure.Migrations
 {
     [DbContext(typeof(FrelanceDbContext))]
-    partial class FrelanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250205071417_ModifyRoles")]
+    partial class ModifyRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
