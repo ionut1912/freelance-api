836fd1c0cb3f9dee56072522326b5ceece75b698
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-09 13:00:52 +0200

    Update database for users

---
+++ Frelance.Infrastructure/Migrations/20250209104756_UpdateUserEntity.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Frelance.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AddressId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ForeignLanguages",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StreetNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProgrammingLanguage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Area = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UsersId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Skills_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Skills",
+                columns: new[] { "Id", "Area", "ProgrammingLanguage", "UsersId" },
+                values: new object[,]
+                {
+                    { 1, "Backend", ".NET", null },
+                    { 2, "Frontend", "Angular", null },
+                    { 3, "Frontend", "JavaScript", null },
+                    { 4, "Frontend", "React", null },
+                    { 5, "Backend", "Python", null },
+                    { 6, "Backend", "Java", null }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_UsersId",
+                table: "Skills",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Addresses_AddressId",
+                table: "AspNetUsers",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Addresses_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ForeignLanguages",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "AspNetUsers");
+        }
+    }
+}
