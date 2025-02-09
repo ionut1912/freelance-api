821cdc2ff94a32aa250a40d024adb364aaaaa11d
nites <nitescucornelionut19@gmail.com>
2025-02-09 12:38:15 +0200

    implement user entity updates

--- Frelance.Infrastructure/Entities/Users.cs
+++ Frelance.Infrastructure/Entities/Users.cs
@@ -4,7 +4,11 @@ namespace Frelance.Infrastructure.Entities;
 
 public class Users:IdentityUser<int>
 {
+    public Address Address { get; set; }
     public List<Projects> Projects { get; set; }
-    public List<ProjectTasks> Tasks { get; set; }=new List<ProjectTasks>();
-    public List<TimeLogs> TimeLogs { get; set; } = new List<TimeLogs>();
+    public List<ProjectTasks> Tasks { get; set; }= [];
+    public List<TimeLogs> TimeLogs { get; set; } = [];
+    public List<Skiills> Skills { get; set; } = [];
+    public List<string> ForeignLanguages { get; set; }
+    public bool IsAvailable{get;set;}
 }
