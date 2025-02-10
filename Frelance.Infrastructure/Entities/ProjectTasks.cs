49c01ce80db34566b17cf74ac22cc5dc71b2c4ec
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-10 19:52:08 +0200

    refactor

--- Frelance.Infrastructure/Entities/ProjectTasks.cs
+++ Frelance.Infrastructure/Entities/ProjectTasks.cs
@@ -1,17 +1,17 @@
-using Frelance.Contracts.Dtos;
 using Frelance.Contracts.Enums;
+
 namespace Frelance.Infrastructure.Entities;
 
 public class ProjectTasks
 {
     public int Id { get; set; }
     public int ProjectId { get; set; }
-    public Projects Projects { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public List<TimeLogs> TimeLogs { get; set; } = [];
+    public required Projects Projects { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public List<TimeLogs> TimeLogs { get; set; } = new();
     public int FreelancerProfileId { get; set; }
-    public FreelancerProfiles FreelancerProfiles { get; set; }
-    public string Status { get; set; }
+    public required FreelancerProfiles FreelancerProfiles { get; set; }
+    public required string Status { get; set; }
     public Priority Priority { get; set; }
 }
--- Frelance.Infrastructure/Entities/Users.cs
+++ Frelance.Infrastructure/Entities/Users.cs
@@ -4,8 +4,8 @@ namespace Frelance.Infrastructure.Entities;
 
 public class Users : IdentityUser<int>
 {
-    public FreelancerProfiles FreelancerProfiles { get; set; }
-    public ClientProfiles ClientProfiles { get; set; }
+    public  FreelancerProfiles FreelancerProfiles { get; set; }
+    public  ClientProfiles ClientProfiles { get; set; }
     public List<Reviews> Reviews { get; set; } = [];
     public List<Proposals> Proposals { get; set; } = [];
 }
