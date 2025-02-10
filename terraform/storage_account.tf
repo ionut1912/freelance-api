9b4b3360e690004e9dd735b56f67bd7ea2223385
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-10 13:24:05 +0200

    fix terraform format

--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage" {
-  name                     = "usersprofiles"  
+  name                     = "usersprofiles"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
