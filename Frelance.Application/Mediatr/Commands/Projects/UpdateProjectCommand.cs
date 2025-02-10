16644b8235cc07b21eddbce928ad6086bb249aca
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-10 19:52:08 +0200

    refactor

--- Frelance.Application/Mediatr/Commands/Projects/UpdateProjectCommand.cs
+++ Frelance.Application/Mediatr/Commands/Projects/UpdateProjectCommand.cs
@@ -2,4 +2,4 @@ using MediatR;
 
 namespace Frelance.Application.Mediatr.Commands.Projects;
 
-public record UpdateProjectCommand(int Id, string Title, string Description, DateTime Deadline, List<string> Technologies, float Budget) : IRequest<Unit>;
+public record UpdateProjectCommand(int Id, string Title, string Description, DateTime Deadline, List<string> Technologies, decimal Budget) : IRequest<Unit>;
--- Frelance.Infrastructure/Entities/FreelancerProfiles.cs
+++ Frelance.Infrastructure/Entities/FreelancerProfiles.cs
@@ -2,15 +2,15 @@ namespace Frelance.Infrastructure.Entities;
 
 public class FreelancerProfiles : BaseUserProfile
 {
-    public List<Projects> Projects { get; set; }
+    public List<Projects> Projects { get; set; } = [];
     public List<ProjectTasks> Tasks { get; set; } = [];
     public List<TimeLogs> TimeLogs { get; set; } = [];
     public List<Skiills> Skills { get; set; } = [];
-    public List<string> ForeignLanguages { get; set; }
+    public List<string> ForeignLanguages { get; set; } = [];
     public bool IsAvailable { get; set; }
-    public string Experience { get; set; }
+    public string? Experience { get; set; }
     public int Rate { get; set; }
-    public string Currency { get; set; }
+    public required string Currency { get; set; }
     public int Rating { get; set; }
-    public string PortofolioUrl { get; set; }
+    public required string PortofolioUrl { get; set; }
 }
