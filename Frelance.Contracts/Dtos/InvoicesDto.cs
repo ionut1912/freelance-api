56e8336e32394fb7bb62cd92d4e52766c7e8960e
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-11 12:24:38 +0200

    Add new columns

--- Frelance.Contracts/Dtos/InvoicesDto.cs
+++ Frelance.Contracts/Dtos/InvoicesDto.cs
@@ -1,3 +1,3 @@
 namespace Frelance.Contracts.Dtos;
 
-public record InvoicesDto(int Id, ProjectDto Project, int ClientId, int FreelancerId, DateOnly Date, decimal Amount, string InvoiceFileUrl);
+public record InvoicesDto(int Id, ProjectDto Project, int ClientId, int FreelancerId, DateOnly Date, decimal Amount, string InvoiceFileUrl,string Status);
--- Frelance.Infrastructure/Entities/ProjectTasks.cs
+++ Frelance.Infrastructure/Entities/ProjectTasks.cs
@@ -6,12 +6,12 @@ public class ProjectTasks
 {
     public int Id { get; set; }
     public int ProjectId { get; set; }
-    public required Projects Projects { get; set; }
-    public required string Title { get; set; }
-    public required string Description { get; set; }
+    public  Projects Projects { get; set; }
+    public  string Title { get; set; }
+    public  string Description { get; set; }
     public List<TimeLogs> TimeLogs { get; set; } = new();
     public int FreelancerProfileId { get; set; }
-    public required FreelancerProfiles FreelancerProfiles { get; set; }
-    public required string Status { get; set; }
+    public  FreelancerProfiles FreelancerProfiles { get; set; }
+    public  string Status { get; set; }
     public string Priority { get; set; }
 }
