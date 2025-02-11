163af47150d0c5e9e1a25e1b6a6ec9e40c49cbd1
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-11 12:24:38 +0200

    Add new columns

--- Frelance.Infrastructure/Entities/Invoices.cs
+++ Frelance.Infrastructure/Entities/Invoices.cs
@@ -4,12 +4,13 @@ public class Invoices
 {
     public int Id { get; set; }
     public int ProjectId { get; set; }
-    public required Projects Project { get; set; }
+    public  Projects Project { get; set; }
     public int ClientId { get; set; }
-    public required ClientProfiles Client { get; set; }
+    public  ClientProfiles Client { get; set; }
     public int FreelancerId { get; set; }
-    public required FreelancerProfiles Freelancer { get; set; }
+    public  FreelancerProfiles Freelancer { get; set; }
     public DateOnly Date { get; set; }
     public decimal Amount { get; set; }
-    public required string InvoiceFileUrl { get; set; }
+    public  string InvoiceFileUrl { get; set; }
+    public string Status { get; set; }
 }
--- Frelance.Infrastructure/Entities/TimeLogs.cs
+++ Frelance.Infrastructure/Entities/TimeLogs.cs
@@ -4,11 +4,11 @@ public class TimeLogs
 {
     public int Id { get; set; }
     public int TaskId { get; set; }
-    public required ProjectTasks Task { get; set; }
+    public  ProjectTasks Task { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
     public DateOnly Date { get; set; }
     public int FreelancerProfileId { get; set; }
-    public required FreelancerProfiles FreelancerProfiles { get; set; }
+    public  FreelancerProfiles FreelancerProfiles { get; set; }
     public int TotalHours { get; set; }
 }
