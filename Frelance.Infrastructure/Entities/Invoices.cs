0063fd988302115580bec91cd47c3b64abf880d7
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-11 12:25:02 +0200

    format code

--- Frelance.Infrastructure/Entities/Invoices.cs
+++ Frelance.Infrastructure/Entities/Invoices.cs
@@ -4,13 +4,13 @@ public class Invoices
 {
     public int Id { get; set; }
     public int ProjectId { get; set; }
-    public  Projects Project { get; set; }
+    public Projects Project { get; set; }
     public int ClientId { get; set; }
-    public  ClientProfiles Client { get; set; }
+    public ClientProfiles Client { get; set; }
     public int FreelancerId { get; set; }
-    public  FreelancerProfiles Freelancer { get; set; }
+    public FreelancerProfiles Freelancer { get; set; }
     public DateOnly Date { get; set; }
     public decimal Amount { get; set; }
-    public  string InvoiceFileUrl { get; set; }
+    public string InvoiceFileUrl { get; set; }
     public string Status { get; set; }
 }
--- Frelance.Infrastructure/Entities/ProjectTasks.cs
+++ Frelance.Infrastructure/Entities/ProjectTasks.cs
@@ -6,12 +6,12 @@ public class ProjectTasks
 {
     public int Id { get; set; }
     public int ProjectId { get; set; }
-    public  Projects Projects { get; set; }
-    public  string Title { get; set; }
-    public  string Description { get; set; }
+    public Projects Projects { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
     public List<TimeLogs> TimeLogs { get; set; } = new();
     public int FreelancerProfileId { get; set; }
-    public  FreelancerProfiles FreelancerProfiles { get; set; }
-    public  string Status { get; set; }
+    public FreelancerProfiles FreelancerProfiles { get; set; }
+    public string Status { get; set; }
     public string Priority { get; set; }
 }
