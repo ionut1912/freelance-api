b345a86f769c9e8f89e126b8f9094f8ae28ea3d0
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-13 10:50:02 +0200

    fix formatting

--- Frelance.Contracts/Dtos/ProjectDto.cs
+++ Frelance.Contracts/Dtos/ProjectDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace Frelance.Contracts.Dtos;
 
-public record ProjectDto(int Id, string Title, string Description, DateTime CreatedAt, DateTime Deadline, List<string> Technologies, decimal Budget, List<TaskDto> Tasks,ClientProfileDto ClientProfileDto,List<ProposalsDto> Proposals,List<ContractsDto> Contracts,List<InvoicesDto> Invoices);
+public record ProjectDto(int Id, string Title, string Description, DateTime CreatedAt, DateTime Deadline, List<string> Technologies, decimal Budget, List<TaskDto> Tasks, ClientProfileDto ClientProfileDto, List<ProposalsDto> Proposals, List<ContractsDto> Contracts, List<InvoicesDto> Invoices);
