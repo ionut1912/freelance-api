de66c918fff602d054e7e9ceb448e7e20a3c104d
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-18 16:18:04 +0200

    remove code comments

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -6,14 +6,6 @@ resource "azurerm_key_vault" "keyvault" {
   tenant_id           = "5c384fed-84cc-44a6-b34a-b060bf102a6e"
 }
 
-/*
-  NOTE: If the access policies already exist in Azure, you must import them into the Terraform state using:
-  
-  terraform import azurerm_key_vault_access_policy.app_service_access /subscriptions/<sub-id>/resourceGroups/freelance-api/providers/Microsoft.KeyVault/vaults/freelance-api-keyvault/objectId/df66e3ec-c251-4294-8587-4721f1ac225e
-
-  terraform import azurerm_key_vault_access_policy.user_access /subscriptions/<sub-id>/resourceGroups/freelance-api/providers/Microsoft.KeyVault/vaults/freelance-api-keyvault/objectId/fe4c5ebe-84a4-4bea-9a09-57fbfe9b0bcb
-*/
-
 resource "azurerm_key_vault_access_policy" "app_service_access" {
   key_vault_id = azurerm_key_vault.keyvault.id
   tenant_id    = azurerm_key_vault.keyvault.tenant_id
