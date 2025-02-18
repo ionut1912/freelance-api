20011b79fcedfdeececbe4305822b90ea3714c13
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-18 15:33:34 +0200

    Revert "fix configs"
    This reverts commit 5eec67dfaeb2a0346f5fcbfdc4f94c595353942b.

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -4,22 +4,16 @@ resource "azurerm_key_vault" "keyvault" {
   location            = azurerm_resource_group.main.location
   sku_name            = "standard"
   tenant_id           = "5c384fed-84cc-44a6-b34a-b060bf102a6e"
-  enable_rbac_authorization = false
 }
 
-
-resource "azurerm_key_vault_access_policy" "terraform_access" {
+resource "azurerm_key_vault_access_policy" "app_service_access" {
   key_vault_id = azurerm_key_vault.keyvault.id
-  tenant_id    = "5c384fed-84cc-44a6-b34a-b060bf102a6e"
-  object_id    = "fe4c5ebe-84a4-4bea-9a09-57fbfe9b0bcb"
+  tenant_id    = azurerm_key_vault.keyvault.tenant_id
+  object_id    = "df66e3ec-c251-4294-8587-4721f1ac225e"
 
   secret_permissions = [
     "Get",
-    "List",
-    "Set",
-    "Delete",
-    "Purge",
-    "Recover"
+    "List"
   ]
 
   key_permissions = [
@@ -32,7 +26,6 @@ resource "azurerm_key_vault_access_policy" "terraform_access" {
     "List"
   ]
 }
-
 
 resource "azurerm_key_vault_secret" "jwt_token_key" {
   name         = "jwt-token-key"
