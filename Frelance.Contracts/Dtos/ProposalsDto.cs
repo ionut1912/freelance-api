eb9821c394523396168097c68893cafe7a223724
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-02-18 11:39:55 +0200

    fix format

--- Frelance.Contracts/Dtos/ProposalsDto.cs
+++ Frelance.Contracts/Dtos/ProposalsDto.cs
@@ -1,3 +1,3 @@
 namespace Frelance.Contracts.Dtos;
 
-public record ProposalsDto(int Id, ProjectDto Project, string Username, decimal ProposedBudget, string Status, DateTime CreatedAt, DateTime? UpdatedAt);
+public record ProposalsDto(int Id,ProjectDto Project,string Username, decimal ProposedBudget, string Status, DateTime CreatedAt, DateTime? UpdatedAt);
