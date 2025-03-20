699f08bc78157e94467cc6db964ec11c7a54d5d5
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-03-20 10:09:54 +0200

    refactoring

--- Frelance.Web/Modules/Utils/ModulesUtils.cs
+++ Frelance.Web/Modules/Utils/ModulesUtils.cs
@@ -5,7 +5,7 @@ namespace Frelance.Web.Modules.Utils;
 
 public static class ModulesUtils
 {
-    public static Role GetRole(HttpContext httpContext)
+    public static Role GetRole(this HttpContext httpContext)
     {
         var role = httpContext.User.FindFirst(ClaimTypes.Role)?.Value;
         var commandRole = role == "Client" ? Role.Client : Role.Freelancer;
