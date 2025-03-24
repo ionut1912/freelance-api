f6072f2d67d2808fe16addd0d6679d993fab8ce2
cornel-ionutnitescu <nitescucornelionut19@gmail.com>
2025-03-24 09:05:21 +0200

    refactor

---
+++ Frelance.Web/Modules/ClientProfileModule.cs
@@ -0,0 +1,25 @@
+using Frelance.Application.Mediatr.Commands.UserProfile;
+using Frelance.Contracts.Enums;
+using Frelance.Contracts.Requests.ClientProfile;
+using Frelance.Web.Modules.Utils;
+using MediatR;
+
+namespace Frelance.Web.Modules;
+
+public static class ClientProfileModule
+{
+    public static void AddClientProfileEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/api/clientProfiles",
+                async (IMediator mediator, CreateClientProfileRequest createClientProfileRequest,
+                    CancellationToken ct) =>
+                {
+                    await mediator.Send(new CreateUserProfileCommand(Role.Client, createClientProfileRequest),
+                        ct);
+                    return Results.Created();
+                })
+            .WithTags("ClientProfiles")
+            .RequireAuthorization("ClientRole");
+    }
+
+}
--- Frelance.Web/Modules/FreelancerProfilesModule.cs
+++ Frelance.Web/Modules/FreelancerProfilesModule.cs
@@ -1,5 +1,7 @@
 using Frelance.Application.Mediatr.Commands.UserProfile;
 using Frelance.Contracts.Dtos;
+using Frelance.Contracts.Enums;
+using Frelance.Contracts.Requests.FreelancerProfiles;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +11,17 @@ public static class FreelancerProfilesModule
 {
     public static void AddFreelancerProfilesEndpoints(this IEndpointRouteBuilder app)
     {
+        app.MapPost("/api/freelancerProfiles",
+                async (IMediator mediator, CreateFreelancerProfileRequest createFreelancerProfileRequest,
+                    CancellationToken ct) =>
+                {
+                    await mediator.Send(new CreateUserProfileCommand(Role.Freelancer, createFreelancerProfileRequest),
+                        ct);
+                    return Results.Created();
+                })
+            .WithTags("FreelancerProfiles")
+            .RequireAuthorization("FreelancerRole");
+        
         app.MapPatch("/api/freelancerProfiles/freelancerDetails/{id:int}", async (IMediator mediator, int id,
                 [FromBody] FreelancerProfileData freelancerProfileData, CancellationToken ct) =>
             {
