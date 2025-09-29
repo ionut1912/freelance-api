832227b7c8b8b8c0727cfcff3a6bf41631e4311e
nites <nitescucornelionut19@gmail.com>
2025-09-29 12:37:13 +0300

    remove unused files

--- terraform/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "resource_group_name" { default = "freelance-api" }
-variable "location" { default = "northeurope" }
-variable "app_service_plan_sku" { default = "B1" }
-variable "sql_sku_name" { default = "Basic" }
-variable "sql_sku_tier" { default = "Basic" }
-variable "sql_storage_mb" { default = 5120 }
